647b43df27e3175a54ae45485461c25924143636
JumaKahiga <kabirumwangi@gmail.com>
2019-09-02 19:01:16 +0300

    Done

---
+++ softmax.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import numpy as np
+import torch
+import helper
+import matplotlib.pyplot as plt
+
+from torchvision import datasets, transforms
+
+
+transform = transforms.Compose([transforms.ToTensor(),
+                               transforms.Normalize((0.5,), (0.5,)),
+                               ])
+trainset = datasets.MNIST('MNIST_data/', download=True, train=True, transform=transform)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+plt.imshow(images[1].numpy().squeeze(), cmap='Greys_r');
+
+
+def activation(x):
+    return 1/(1+torch.exp(-x))
+
+# Flatten input images
+inputs = images.view(images.shape[0], -1)
+
+# Create parameters
+w1 = torch.randn(784, 256)
+b1 = torch.randn(256)
+
+w2 = torch.randn(256, 10)
+b2 = torch.randn(10)
+
+h = activation(torch.mm(inputs, w1) + b1)
+
+out = torch.mm(h, w2) + b2
+
+
+def softmax(x):
+    return torch.exp(x)/torch.sum(torch.exp(x), dim=1).view(-1, 1)
+
+
+probabilities = softmax(out)
+
+print(probabilities.shape)
+
+
+
+
